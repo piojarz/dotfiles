0abddc01810600bbe0cae7b387e1f419f93a9e1e
Piotr Jarzabek <piojarz@gmail.com>
2026-01-29 22:57:45 +0100

    Fix ripgrep config path to avoid store path collision

--- modules/common/home/tools/ripgrep.nix
+++ modules/common/home/tools/ripgrep.nix
@@ -7,8 +7,9 @@
     # Link the existing ripgrep configuration
   };
 
-  home.file.".config/ripgrep/config" = {
-    source = ../../../../config/common/ripgrep/config;
+  home.file.".config/ripgrep" = {
+    source = ../../../../config/common/ripgrep;
+    recursive = true;
   };
 
   home.packages = with pkgs; [
